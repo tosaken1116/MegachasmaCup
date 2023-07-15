41d03f00aa3d0eb394c8cd47c2b3a1fb409869cc
012xx <night.fox.045@gmail.com>
2023-07-15 08:09:11 +0000

    Create: GetTags関数の作成

--- graph/services/tag.go
+++ graph/services/tag.go
@@ -1,7 +1,28 @@
 package services
 
-import "gorm.io/gorm"
+import (
+	"context"
+	"megachasma/graph/model"
+	dbModel "megachasma/graph/model/db"
+
+	"gorm.io/gorm"
+)
 
 type tagService struct {
 	db *gorm.DB
+}
+
+func (ts *tagService) GetTags(ctx context.Context, searchWord string) (*model.Tag, error) {
+	tags := dbModel.Tag{}
+	if err := ts.db.Find(&tags).Error; err != nil {
+		return nil, err
+	}
+	return convertTag(tags), nil
+}
+
+func convertTag(tags dbModel.Tag) *model.Tag {
+	return &model.Tag{
+		ID:   tags.ID,
+		Name: tags.Name,
+	}
 }
