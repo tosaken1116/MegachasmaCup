754227e02381e7e8e1daa994534288251e8fccb2
012xx <night.fox.045@gmail.com>
2023-07-16 05:28:27 +0000

    Fix: UpdateSchoolの実装

--- graph/services/school.go
+++ graph/services/school.go
@@ -47,18 +47,28 @@ func (ss *schoolService) CreateSchool(ctx context.Context, Name string, OwnerID 
 	return createdSchool, nil
 }
 
-// func (ss *schoolService) UpdateSchool(input model.NewSchool) (*model.School, error) {
-// 	school, err := GetSchoolByID(ss.db, input.ID)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	school.Name = input.Name
-// 	if err := ss.db.Save(&school).Error; err != nil {
-// 		return nil, err
-// 	}
-// 	updatedSchool := convertSchool(*school)
-// 	return updatedSchool, nil
-// }
+func (ss *schoolService) UpdateSchool(ctx context.Context, Name string, OwnerID string) (*model.School, error) {
+	pOwnerID, err := uuid.Parse(OwnerID)
+	if err != nil {
+		return nil, err
+	}
+	school := dbModel.School{
+		Name:    Name,
+		OwnerID: pOwnerID,
+	}
+	if err := ss.db.Where("id = ?", OwnerID).Find(&school).Error; err != nil {
+		return nil, err
+	}
+
+	school.Name = Name
+
+	if err := ss.db.Save(&school).Error; err != nil {
+		return nil, err
+	}
+
+	updatedSchool := convertSchool(school)
+	return updatedSchool, nil
+}
 
 func (ss *schoolService) GetSchoolBySearchWord(searchWord string) ([]*model.School, error) {
 	schools := []*dbModel.School{}
