549d6fb7a2059575cf26e5f29f1958ad723917b3
tosaken1116 <nagato20021122@gmail.com>
2023-07-17 14:48:53 +0900

    add:cors

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/99designs/gqlgen v0.17.34
 	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/google/uuid v1.3.0
+	github.com/rs/cors v1.9.0
 	github.com/subosito/gotenv v1.4.2
 	github.com/vektah/gqlparser/v2 v2.5.4
 	golang.org/x/crypto v0.8.0
--- server.go
+++ server.go
@@ -13,6 +13,7 @@ import (
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/rs/cors"
 )
 
 const defaultPort = "8080"
@@ -34,7 +35,11 @@ func main() {
 	}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", auth.AuthMiddleware(srv))
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowCredentials: true,
+	})
+	http.Handle("/query", c.Handler(auth.AuthMiddleware(srv)))
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
